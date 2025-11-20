ab8dc1c535502d148d7d6819e3607e60cf1b2fec
MuhammedZeki <muhammed.codes@gmail.com>
2025-11-20 19:11:31 +0300

    fixed

--- src/hooks/LanguageContext.jsx
+++ src/hooks/LanguageContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useEffect, useState } from "react";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const LanguageContext = createContext();
 
 export const LanguageContextProvider = ({ children }) => {
